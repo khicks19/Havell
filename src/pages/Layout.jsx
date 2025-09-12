b0030390c54d151992204b0d4215aef8724b14c8
khicks19 <kalebhicks.havell@gmail.com>
2025-09-11 20:38:02 -0700

    Update Layout.jsx

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -10,7 +10,7 @@ export default function Layout(){
       <header className="sticky top-0 z-40 border-b bg-white/90 backdrop-blur">
         <div className="mx-auto flex max-w-7xl items-center justify-between px-6 py-4 sm:px-8">
        <Link to="/" className="flex items-center" aria-label="Havell Home">
-  <LogoImg className="h-21 sm:h-25 w-auto select-none" />
+  <LogoImg className="h-20 sm:h-24 w-auto select-none" />
 </Link>
 
 
